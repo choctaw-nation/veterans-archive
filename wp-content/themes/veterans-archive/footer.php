1fe2b8a184703bb2d4c1198abac24869d02671a7
KJ Roelke <kj.roelke@gmail.com>
2024-01-24 13:42:12 -0600

    update footer styles

--- wp-content/themes/veterans-archive/footer.php
+++ wp-content/themes/veterans-archive/footer.php
@@ -8,60 +8,42 @@
 
 ?>
 
-<footer class="footer bg-primary py-5 container-fluid gx-5 text-white text-center d-flex flex-column align-items-center">
+<footer class="footer container-fluid">
 	<div class="row">
-		<?php
-		if ( has_nav_menu( 'footer_menu' ) ) {
-			wp_nav_menu(
-				array(
-					'theme_location'  => 'footer_menu',
-					'menu_class'      => 'footer-nav list-unstyled navbar-nav flex-row',
-					'container'       => 'nav',
-					'container_class' => 'navbar',
-				)
-			);
-		}
-		?>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-4">
-				<a href="<?php echo esc_url( site_url() ); ?>" class="logo">
-					<figure class="logo-img d-inline-block">
-						<span aria-label="to Home Page">
-							<?php echo bloginfo( 'name' ); ?>
-						</span>
-					</figure>
-				</a>
-				<div class="row">
-					<div class="social-icons">
-						<?php
-						$socials = array(
-							array(
-								'icon_class' => 'fa-brands fa-facebook-f',
-								'href'       => 'https://facebook.com/',
-								'aria-label' => 'Follow Us on Facebook',
-							),
-							array(
-								'icon_class' => 'fa-brands fa-instagram',
-								'href'       => 'https://instagram.com',
-								'aria-label' => 'Follow Us on Instagram',
-							),
-						);
-						?>
-						<?php foreach ( $socials as $social ) : ?>
-						<a href="<?php echo $social['href']; ?>" class="social" target="_blank" rel="noopener noreferrer" aria-label="<?php echo $social['aria-label']; ?>">
-							<i class="<?php echo "text-white fa-3x {$social['icon_class']}"; ?>"></i>
-						</a>
-						<?php endforeach; ?>
-					</div>
-				</div>
-			</div>
-			<div class="col"></div>
+		<div class="col-auto flex-grow-1 col-sm-6 col-lg-4 bg-green py-5">
+			<a href="<?php echo esc_url( site_url() ); ?>" class="logo">
+				<span aria-label="to Home Page" class='fs-1 text-uppercase'>
+					<?php echo bloginfo( 'name' ); ?>
+				</span>
+			</a>
 		</div>
-		<div class="row my-5">
-			<div id="copyright">
-				<?php echo '&copy;&nbsp;' . gmdate( 'Y' ) . '&nbsp;Choctaw Nation of Oklahoma.'; ?>
+		<div class="col-2 footer-split d-none d-lg-block"></div>
+		<div class="col-auto flex-grow-1 col-sm-6 bg-primary py-5 text-sm-end">
+			<div class="social-icons">
+				<?php
+				$socials = array(
+					array(
+						'icon_class' => 'fa-brands fa-facebook-f',
+						'href'       => 'https://facebook.com/',
+						'aria-label' => 'Follow Us on Facebook',
+					),
+					array(
+						'icon_class' => 'fa-brands fa-instagram',
+						'href'       => 'https://instagram.com',
+						'aria-label' => 'Follow Us on Instagram',
+					),
+				);
+				?>
+				<?php foreach ( $socials as $social ) : ?>
+				<a href="<?php echo $social['href']; ?>" class="social" target="_blank" rel="noopener noreferrer" aria-label="<?php echo $social['aria-label']; ?>">
+					<i class="<?php echo "text-white fa-3x {$social['icon_class']}"; ?>"></i>
+				</a>
+				<?php endforeach; ?>
+			</div>
+			<div class="row mt-3 w-75 justify-content-end ms-auto">
+				<div id="copyright" class="text-white">
+					<?php echo '&copy;&nbsp;' . gmdate( 'Y' ) . '&nbsp;Choctaw Nation of Oklahoma. All Rights Reserved.'; ?>
+				</div>
 			</div>
 		</div>
 	</div>
