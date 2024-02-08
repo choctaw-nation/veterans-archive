4798e6480d3db824b2805a9202439a0e19308c42
KJ Roelke <kj.roelke@gmail.com>
2024-02-08 14:16:41 -0600

    update button style for two-col template

--- wp-content/themes/veterans-archive/template-parts/content-two-col.php
+++ wp-content/themes/veterans-archive/template-parts/content-two-col.php
@@ -6,12 +6,14 @@
  */
 
 use ChoctawNation\ACF\Image;
+use ChoctawNation\Components\Buttons;
 use ChoctawNation\Components\Divider;
 
 $divider  = new Divider();
 $acf      = $args['acf'];
 $reversed = $args['reversed'] ?? false;
 $image    = new Image( $acf['image'], 'front-page-thumb' );
+$buttons  = new Buttons();
 
 ?>
 <div class="row row-cols-1 row-cols-lg-2 <?php echo ( $reversed ? ' flex-row-reverse' : '' ); ?>">
@@ -32,12 +34,21 @@ $image    = new Image( $acf['image'], 'front-page-thumb' );
 		<?php
 		$direction = $reversed ? 'start' : 'end';
 		$divider->the_divider( $direction );
-		echo acf_esc_html( $acf['subheadline'] );
+		echo "<div class='mb-auto'>" . acf_esc_html( $acf['subheadline'] ) . '</div>';
 		?>
-		<?php if ( $acf['link'] ) : ?>
-		<a href='<?php echo esc_url( $acf['link']['url'] ); ?>' class='btn btn-primary' target="<?php echo esc_attr( $acf['link']['target'] ); ?>">
-			<?php echo esc_textarea( $acf['link']['title'] ); ?>
-		</a>
+		<?php
+		if ( $acf['link'] ) :
+			$buttons->the_button(
+				array(
+					'element' => 'a',
+					'href'    => esc_url( $acf['link']['url'] ),
+					'class'   => 'btn-outline-primary',
+					'target'  => $acf['link']['target'],
+					'text'    => esc_textarea( $acf['link']['title'] ),
+				),
+				'align-self-start mt-3'
+			);
+			?>
 		<?php endif; ?>
 	</div>
 </div>
