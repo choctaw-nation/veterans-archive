7e2c4687ba055c9aa06a2070caf9d5eb19272c92
KJ Roelke <kj.roelke@gmail.com>
2024-02-08 15:28:10 -0600

    update build scripts

--- wp-content/themes/veterans-archive/dist/veteranSubmission.asset.php
+++ wp-content/themes/veterans-archive/dist/veteranSubmission.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '6e771fddfb55a0541ca6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '25541ea8cb9547b7b033');
