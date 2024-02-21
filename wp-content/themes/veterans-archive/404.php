452cdbd98ceed289f496300c56a4f01fa5183ad8
KJ Roelke <kj.roelke@gmail.com>
2024-02-21 16:15:03 -0600

    update 404 page style

--- wp-content/themes/veterans-archive/404.php
+++ wp-content/themes/veterans-archive/404.php
@@ -5,7 +5,7 @@
  * @package ChoctawNation
  */
 
-get_header();
+get_header( 'search' );
 ?>
 <main class="mt-5 pt-5 bg-light">
 	<section class="container">
@@ -13,13 +13,6 @@ get_header();
 			<div class="col">
 				<h1 class='display-1'>404</h1>
 				<p class='display-4'>Page not found.</p>
-			</div>
-		</div>
-	</section>
-	<section class="bg-dark-blue py-5 my-3">
-		<div class="container">
-			<div class="row">
-				<?php get_template_part( 'template-parts/search', 'search-bar', array( 'with_browse' => true ) ); ?>
 			</div>
 		</div>
 	</section>
