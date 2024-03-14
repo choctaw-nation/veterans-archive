aef09d9610c9bb34ced069816dc1da8e6bf64d42
KJ Roelke <kj.roelke@gmail.com>
2024-03-14 12:20:16 -0500

    style Gravity forms submit button Fixes #60

--- wp-content/themes/veterans-archive/dist/global.asset.php
+++ wp-content/themes/veterans-archive/dist/global.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'f45fc66ffa33f43c1eb7');
+<?php return array('dependencies' => array(), 'version' => '06084742962750106644');
--- wp-content/themes/veterans-archive/page-templates/blank-container.php
+++ wp-content/themes/veterans-archive/page-templates/blank-container.php
@@ -8,7 +8,7 @@
 get_header( 'search' );
 get_template_part( 'template-parts/section', 'hero' );
 ?>
-<div class="container">
+<div class="container my-5 py-5">
 	<?php echo get_field( 'page_content' ); ?>
 </div>
 <?php
