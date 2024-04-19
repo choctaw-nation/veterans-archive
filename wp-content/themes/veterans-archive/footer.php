30f9d80ec2fbac2ea3bc06efe1ea6da0b3b4e548
KJ Roelke <kj.roelke@gmail.com>
2024-04-19 18:44:49 -0500

    Social icons showing when not set Fixes #70

--- wp-content/themes/veterans-archive/footer.php
+++ wp-content/themes/veterans-archive/footer.php
@@ -18,32 +18,7 @@ $footer_col_classes = 'col-auto flex-grow-1 py-5 col-sm-6 d-flex flex-column jus
 		</div>
 		<div class="col-2 footer-split d-none d-lg-block"></div>
 		<div class="<?php echo $footer_col_classes; ?> pe-5 bg-primary text-sm-end align-items-sm-end">
-			<div class="social-icons me-5 d-none">
-				<?php
-				$socials = array(
-					array(
-						'icon_class' => 'fa-brands fa-x-twitter',
-						'href'       => 'https://twitter.com/',
-						'aria-label' => 'Follow Us on X (Twitter)',
-					),
-					array(
-						'icon_class' => 'fa-brands fa-facebook-f',
-						'href'       => 'https://facebook.com/',
-						'aria-label' => 'Follow Us on Facebook',
-					),
-					array(
-						'icon_class' => 'fa-brands fa-instagram',
-						'href'       => 'https://instagram.com',
-						'aria-label' => 'Follow Us on Instagram',
-					),
-				);
-				?>
-				<?php foreach ( $socials as $social ) : ?>
-				<a href="<?php echo $social['href']; ?>" class="social mx-2" target="_blank" rel="noopener noreferrer" aria-label="<?php echo $social['aria-label']; ?>">
-					<i class="<?php echo "fa-2xl {$social['icon_class']}"; ?>"></i>
-				</a>
-				<?php endforeach; ?>
-			</div>
+			<?php // get_template_part( 'template-parts/footer', 'socials' ); ?>
 			<div id="copyright" class="mt-3 me-5 w-75 text-dark">
 				<?php echo '&copy;&nbsp;' . gmdate( 'Y' ) . '&nbsp;Choctaw Nation of Oklahoma. All Rights Reserved.'; ?>
 			</div>
